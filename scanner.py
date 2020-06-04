603b88af5a72f365281a3b75f4c1109295aa4683
Noppik <55311174+Noppik@users.noreply.github.com>
2020-06-04 08:52:03 +0500

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -3,12 +3,13 @@ import sys
 
 start = int(sys.argv[1])
 end = int(sys.argv[2])
+ip = sys.argv[3]
 print('Checking:')
 for port in range(start, end):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(0.1)
     try:
-        con = s.connect(('localhost', port))
+        con = s.connect((ip, port))
         print('Port ' + str(port) + ' open')
         con.close()
     except:
